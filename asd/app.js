277d00ed3eee3830f36c217f6467e5e5b71286d2
abdulsamet6634 <127586720+abdulsamet6634@users.noreply.github.com>
2023-03-11 10:20:55 +0300

    Add files via upload

---
+++ asd/app.js
@@ -0,0 +1,23 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    dots:true,
+    autoplay:true,
+    center:true,
+    autoplayTimeout:3000 , 
+    
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+$(window).scroll(function () { 
+    $('nav').toggleClass('scrolled',$(this).scrollTop()>50);
+});
